c0f107cc33155e774d0444bdfa949dde299b7ee5
Marlena Tsang <Marlena345@gmail.com>
2022-05-23 18:08:08 -0400

    Completed assignment

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,4 @@
+//reduce function called changedState
 function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
@@ -11,3 +12,16 @@ let state = { count: 0 };
 let action = { type: "counter/increment" };
 
 changeState(state, action);
+
+//renders our state
+function render(){
+  const app = document.querySelector("#app");
+  app.textContent = state.count;
+}
+
+//presisting state 
+function dispatch(action){
+  state = changeState(state,action);
+  render();
+}
+render();
